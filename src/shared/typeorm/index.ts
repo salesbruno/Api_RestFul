3b0d96f4eee804ee5b6eff3e8a586a50944a08a4
Bruno Sales <salesdasilvabruno@gmail.com>
2023-07-20 20:03:03 -0300

    Create migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1688398469454 } from './migrations/1688398469454-CreateRolesTable'
 import { Role } from '@roles/entities/Role'
+import { CreateUsersTable1689893194624 } from './migrations/1689893194624-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1688398469454],
+  migrations: [CreateRolesTable1688398469454, CreateUsersTable1689893194624],
 })
