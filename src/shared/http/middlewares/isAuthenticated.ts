d117a6a010c7d3b9126a85c3ceed0080267f61df
Bruno Sales <salesdasilvabruno@gmail.com>
2023-08-18 19:46:17 -0300

    Refactor middleware authenticated

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,9 @@ import { NextFunction, Request, Response } from 'express'
 import { Secret, verify } from 'jsonwebtoken'
 import authConfig from '@config/auth'
 
+type JwtPayloadProps = {
+  sub: string
+}
 export const isAuthenticated = (
   req: Request,
   res: Response,
@@ -14,7 +17,9 @@ export const isAuthenticated = (
   }
   const token = authHeader.replace('Bearer ', '')
   try {
-    verify(token, authConfig.jwt.secret as Secret)
+    const decodedToken = verify(token, authConfig.jwt.secret as Secret)
+    const { sub } = decodedToken as JwtPayloadProps
+    req.user = { id: sub }
     return next()
   } catch {
     throw new AppError('Invalid authentication toke', 401)
