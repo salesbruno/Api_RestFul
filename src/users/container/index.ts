91cc08b0c337d563a3225030ed3146fd971deb19
Bruno Sales <salesdasilvabruno@gmail.com>
2023-09-06 21:11:42 -0300

    Create ShowProfile router

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,8 +1,9 @@
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
 import { UsersRepository } from '@users/repositories/UsersRepository'
 import { CreateLoginController } from '@users/useCases/createLogin/CreateLoginController'
-import { CreateUserController } from '@users/useCases/showProfile/ShowProfileController'
+import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
 import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
+import { ShowProfileController } from '@users/useCases/showProfile/ShowProfileController'
 import { UpdateAvatarController } from '@users/useCases/updateAvatar/UpdateAvatarController'
 import { container } from 'tsyringe'
 
@@ -11,6 +12,7 @@ container.registerSingleton<IUsersRepository>(
   UsersRepository,
 )
 container.registerSingleton('CreateUserController', CreateUserController)
-container.registerSingleton('ListUsersController', ListUsersController)
+container.registerSingleton('ListUsersControllers', ListUsersController)
 container.registerSingleton('CreateLoginController', CreateLoginController)
 container.registerSingleton('UpdateAvatarController', UpdateAvatarController)
+container.registerSingleton('ShowProfileController', ShowProfileController)
--- src/users/http/users.routes.ts
+++ src/users/http/users.routes.ts
@@ -2,18 +2,20 @@ import { Router } from 'express'
 import { celebrate, Joi, Segments } from 'celebrate'
 import { container } from 'tsyringe'
 import multer from 'multer'
-import { CreateUserController } from '@users/useCases/showProfile/ShowProfileController'
+import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
 import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
 import { CreateLoginController } from '@users/useCases/createLogin/CreateLoginController'
 import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated'
 import uploadConfig from '@config/upload'
 import { UpdateAvatarController } from '@users/useCases/updateAvatar/UpdateAvatarController'
+import { ShowProfileController } from '@users/useCases/showProfile/ShowProfileController'
 
 const usersRouter = Router()
 const createUserController = container.resolve(CreateUserController)
 const listUsersController = container.resolve(ListUsersController)
 const createLoginController = container.resolve(CreateLoginController)
 const updateAvatarController = container.resolve(UpdateAvatarController)
+const showProfileController = container.resolve(ShowProfileController)
 const upload = multer(uploadConfig)
 
 usersRouter.post(
@@ -32,7 +34,6 @@ usersRouter.post(
     return createUserController.handle(request, response)
   },
 )
-
 usersRouter.get(
   '/',
   isAuthenticated,
@@ -46,7 +47,6 @@ usersRouter.get(
     return listUsersController.handle(request, response)
   },
 )
-
 usersRouter.post(
   '/login',
   celebrate({
@@ -59,7 +59,6 @@ usersRouter.post(
     return createLoginController.handle(request, response)
   },
 )
-
 usersRouter.patch(
   '/avatar',
   isAuthenticated,
@@ -68,5 +67,9 @@ usersRouter.patch(
     return updateAvatarController.handle(request, response)
   },
 )
+
+usersRouter.get('/profile', isAuthenticated, (request, response) => {
+  return showProfileController.handle(request, response)
+})
 
 export { usersRouter }
