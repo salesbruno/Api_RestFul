3dc35d38657cf4ac497ba1477e0fd63022105e3f
Bruno Sales <salesdasilvabruno@gmail.com>
2023-09-09 16:27:21 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
+import { Role } from '@roles/entities/Role'
+import { User } from '@users/entities/User'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1688398469454 } from './migrations/1688398469454-CreateRolesTable'
-import { Role } from '@roles/entities/Role'
 import { CreateUsersTable1689893194624 } from './migrations/1689893194624-CreateUsersTable'
 import { AddRoleIdToUsersTable1689895295839 } from './migrations/1689895295839-AddRoleIdToUsersTable'
-import { User } from '@users/entities/User'
+import { CreateRefreshTokensTable1694286668460 } from './migrations/1694286668460-CreateRefreshTokensTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
@@ -13,5 +14,7 @@ export const dataSource = new DataSource({
     CreateRolesTable1688398469454,
     CreateUsersTable1689893194624,
     AddRoleIdToUsersTable1689895295839,
+    ,
+    CreateRefreshTokensTable1694286668460,
   ],
 })
