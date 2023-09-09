083fe5f42e37c0b718641125459b0464b5528378
Bruno Sales <salesdasilvabruno@gmail.com>
2023-09-09 16:45:54 -0300

    Create RefreshToken entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -5,11 +5,12 @@ import { CreateRolesTable1688398469454 } from './migrations/1688398469454-Create
 import { CreateUsersTable1689893194624 } from './migrations/1689893194624-CreateUsersTable'
 import { AddRoleIdToUsersTable1689895295839 } from './migrations/1689895295839-AddRoleIdToUsersTable'
 import { CreateRefreshTokensTable1694286668460 } from './migrations/1694286668460-CreateRefreshTokensTable'
+import { RefreshToken } from '@users/entities/RefreshToken'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role, User],
+  entities: [Role, User, RefreshToken],
   migrations: [
     CreateRolesTable1688398469454,
     CreateUsersTable1689893194624,
